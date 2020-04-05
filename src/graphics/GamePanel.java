c707d21f2568db476173a1d1471bd125742d5fdd
jharmon8 <jharmon@caltech.edu>
2020-04-05 16:38:50 -0700

    Add a blank JFrame and JPanel

---
+++ src/graphics/GamePanel.java
@@ -0,0 +1,55 @@
+package graphics;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements KeyListener {
+
+	private static final long serialVersionUID = 1L;
+	private int S_WIDTH = 1024;
+	private int S_HEIGHT = 1024;
+	
+	public GamePanel() {
+		addKeyListener(this);
+		setPreferredSize(new Dimension(S_WIDTH, S_HEIGHT));
+		
+		setFocusable(true);
+		requestFocus();
+		
+		repaint();
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		// clear screen
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, S_WIDTH, S_HEIGHT);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			break;
+		case KeyEvent.VK_A:
+			break;
+		case KeyEvent.VK_S:
+			break;
+		case KeyEvent.VK_D:
+			break;
+		case KeyEvent.VK_SPACE:
+			break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
+}
