dcc839ebf3ff2b75d03724d88b425d143b064b34
jakeharmon8 <jakeharmon8@gmail.com>
2020-04-27 19:57:42 -0700

    Make a working HTTP request

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,23 @@
-import graphics.GameFrame;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Driver {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		//GameFrame.launchGame();
+		Socket s = new Socket(InetAddress.getByName("joshuaharmon.io"), 80);
+		PrintWriter pw = new PrintWriter(s.getOutputStream());
+		pw.println("GET / HTTP/1.1");
+		pw.println("Host: joshuaharmon.io");
+		pw.println("");
+		pw.flush();
+		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		String t;
+		while((t = br.readLine()) != null) System.out.println(t);
+		br.close();
 	}
 }
