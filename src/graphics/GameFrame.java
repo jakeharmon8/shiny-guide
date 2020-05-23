bb8028253d2becaad50a55875ae209997558f366
jakeharmon8 <jakeharmon8@gmail.com>
2020-05-23 12:58:34 -0700

    Change frame text to ShinySnake

--- src/graphics/GameFrame.java
+++ src/graphics/GameFrame.java
@@ -6,7 +6,7 @@ public class GameFrame {
 
 	public static void launchGame() {
 		// TODO Auto-generated method stub
-		JFrame f = new JFrame("ShinyPewPew v1.0");
+		JFrame f = new JFrame("ShinySnake v1.0");
 		GamePanel p = new GamePanel();
 		f.add(p);
 		f.pack();
